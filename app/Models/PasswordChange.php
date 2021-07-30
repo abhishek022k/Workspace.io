4d9792b74effd166ee68c4b3f703e8843e5947c5
Abhishek022k <abhishek022k@gmail.com>
2021-07-30 11:59:19 +0530

    SoftDeletes to password changes

--- app/Models/PasswordChange.php
+++ app/Models/PasswordChange.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PasswordChange extends Model
 {
@@ -15,6 +16,7 @@ class PasswordChange extends Model
         'token', 'user_id','expiry_date'
     ];
     public $timestamps = false;
+    use SoftDeletes;
 
     /**
      * The attributes excluded from the model's JSON form.
